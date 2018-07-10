ecfce3fd40f43c5581a237958b824c7ac659bcd3
Ryan O'Leary <ryanoleary@google.com>
2018-07-10 16:30:41 -0700

    Permit arguments with embedded spaces

--- expect.go
+++ expect.go
@@ -854,7 +854,7 @@ func SpawnFake(b []Batcher, timeout time.Duration, opt ...Option) (*GExpect, <-c
 
 // Spawn starts a new process and collects the output. The error channel returns the result of the
 // command Spawned when it finishes.
-func Spawn(command string, timeout time.Duration, opts ...Option) (*GExpect, <-chan error, error) {
+func Spawn(args []string, timeout time.Duration, opts ...Option) (*GExpect, <-chan error, error) {
 	pty, err := term.OpenPTY()
 	if err != nil {
 		return nil, nil, err
@@ -867,7 +867,6 @@ func Spawn(command string, timeout time.Duration, opts ...Option) (*GExpect, <-c
 		timeout = DefaultTimeout
 	}
 	// Get the command up and running
-	args := strings.Fields(command)
 	cmd := exec.Command(args[0], args[1:]...)
 	// This ties the commands Stdin,Stdout & Stderr to the virtual terminal we created
 	cmd.Stdin, cmd.Stdout, cmd.Stderr = pty.Slave, pty.Slave, pty.Slave
